fbe8410c99deb744ca4cc2ba72db10aad70b3d5d
Ahmad Abdullah <ahmadnofly@gmail.com>
2022-05-04 19:27:20 +0300

    add springboot

---
+++ class_notes/29-11-2022/notes/staff-registration/src/test/java/com/hrm/staffregistration/StaffRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrm.staffregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
